b2ff28fb4fde3f6d0c2e0d91b682433fe3a8ce23
Dmitry Burnaev <dmitry.burnaev@getorca.app>
2021-06-20 16:08:24 +0300

    #1: removed previous project's version

--- src/podcast/ContentView.swift
+++ src/podcast/ContentView.swift
@@ -41,7 +41,7 @@ struct PodcastRow: View {
                 .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
             VStack (alignment: .leading){
                 Text(podcast.name).font(.headline).baselineOffset(5)
-                Text(podcast.description).font(.subheadline).lineLimit(2)
+                Text(podcast.description).font(.subheadline).lineLimit(3)
             }.padding(.leading, 8)
         }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
     }
