eb625c8b36568c1a78d5ea4bb3b7ebf12d6752ab
Dmitry Burnaev <dmitry.burnaev@getorca.app>
2021-06-20 15:53:13 +0300

    #1: Small visual updates for  podcast's list view

--- src/podcast/ContentView.swift
+++ src/podcast/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  podcast
-//
-//  Created by Dmitry Burnaev on 12.06.2021.
-//
-
 import SwiftUI
 
 
@@ -19,29 +12,38 @@ struct ContentView: View {
     
     let podcasts: [Podcast] = [
         .init(id: 1, name: "Audio Books", description: "Audio books for everyone", imageURL: "podcast-cax7j52Xha3f"),
-        .init(id: 2, name: "Listen later", description: "Interesting auidios", imageURL: "podcast-75fc068c8ecbca77"),
-        .init(id: 3, name: "Test podcast", description: "Only for testing new features", imageURL: "podcast-98765f61b45b1503"),
-        .init(id: 4, name: "Lighting talks", description: "Some audio for listening after difficult day", imageURL: "cover-default"),
+        .init(id: 2, name: "Listen later", description: "Only interesting audios", imageURL: "podcast-75fc068c8ecbca77"),
+        .init(id: 3, name: "Lighting talks", description: "Some audio for listening after difficult day", imageURL: "cover-default"),
+        .init(id: 4, name: "Test podcast", description: "SwiftUI gives us five built-in shapes that are commonly used: rectangle, rounded rectangle, circle, ellipse, and capsule. The last three in particular are subtly different in how they behave based on what sizes you provide, but we can demonstrate all the options with a single example", imageURL: "podcast-98765f61b45b1503"),
     ]
     
     var body: some View {
         NavigationView{
             List {
-                Text("Podcast list will be here").font(.title2)
                 ForEach(podcasts, id: \.id){ podcast in
-                    HStack{
-                        Image(podcast.imageURL)
-                            .resizable()
-                            .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .clipped()
-                        VStack (alignment: .leading){
-                            Text(podcast.name).font(.headline)
-                            Text(podcast.description).font(.subheadline)
-                        }
-                    }
+                    PodcastRow(podcast: podcast)
                 }
             }.navigationBarTitle(Text("Podcasts"))
         }
+    }
+}
+
+
+struct PodcastRow: View {
+    let podcast: Podcast
+    
+    var body: some View{
+        HStack{
+            Image(podcast.imageURL)
+                .resizable()
+                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                .overlay(RoundedRectangle(cornerRadius: 10, style: .continuous).stroke(Color.black, lineWidth: 1))
+                .frame(width: 70, height: 70, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            VStack (alignment: .leading){
+                Text(podcast.name).font(.headline).baselineOffset(5)
+                Text(podcast.description).font(.subheadline).lineLimit(2)
+            }.padding(.leading, 8)
+        }.padding(.init(top: 12, leading: 0, bottom: 12, trailing: 0))
     }
 }
 
