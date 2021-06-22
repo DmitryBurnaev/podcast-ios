b65fc96bf1fe9c61a9d48a1f4f90440f3208be03
Dmitry Burnaev <dmitry.burnaev@getorca.app>
2021-06-22 09:19:28 +0300

    #3: Tabviews refactoring

---
+++ src/podcast/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  HomeView.swift
+//  podcast
+//
+//  Created by Dmitry Burnaev on 22.06.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView{
+            Text("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
