b4ce31317c7a77c8ce520975f9f324e4b3ecc261
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-07-20 16:30:38 +0300

    Added podcasts to the main view

--- src/podcast/HomeView.swift
+++ src/podcast/HomeView.swift
@@ -1,13 +1,45 @@
-//
-//  HomeView.swift
-//  podcast
-//
-//  Created by Dmitry Burnaev on 22.06.2021.
-//
-
 import SwiftUI
 
+
+struct PodcastsList: Decodable{
+    let payload: [PodcastItem]
+}
+
+struct PodcastItem: Decodable, Hashable {
+    let id: Int
+    let name, image_url: String
+}
+
+class GridViewModel: ObservableObject{
+    @Published var podcasts = 0..<5
+    @Published var payload = [PodcastItem]()
+    
+
+    init() {
+//        Timer.scheduledTimer(withTimeInterval: 2, repeats: false){(_) in
+//            self.podcasts = 0..<15
+//        }
+//
+        guard let url = URL(string: "http://192.168.1.6:8001/api/podcasts/") else {
+            return
+        }
+        URLSession.shared.dataTask(with: url){ (data, resp, err) in
+            guard let data = data else { return }
+            do {
+                let res = try JSONDecoder().decode(PodcastsList.self, from: data)
+                self.payload = res.payload
+            } catch {
+                print("Failed to decode: \(err) \(data)")
+            }
+        }.resume()
+    
+    }
+}
+
 struct HomeView: View {
+    
+    @ObservedObject var vm = GridViewModel()
+    
     var body: some View {
         NavigationView{
             ScrollView{
@@ -16,7 +48,7 @@ struct HomeView: View {
                     GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12),
                     GridItem(.flexible(minimum: 100, maximum: 200)),
                 ], spacing: 12, content: {
-                    ForEach(0..<20, id: \.self){ num in
+                    ForEach(vm.payload, id: \.self){ num in
                         VStack(alignment: .leading){
                             Spacer()
                                 .frame(width: 90, height: 90)
@@ -29,7 +61,7 @@ struct HomeView: View {
 
                     }
                 }).padding(.horizontal, 15)
-            }.navigationTitle("Favorite podcast")
+            }.navigationTitle("Your podcasts")
         }
     }
 }
