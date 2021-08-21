6f5bad21b78c2bb732ee36f70fe3946727ac4390
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-08-21 13:46:58 +0300

    [#21] Added interceptor in order to attach auth header to each request

--- src/podcast/Services/WebService.swift
+++ src/podcast/Services/WebService.swift
@@ -2,6 +2,8 @@ import Foundation
 import KeychainAccess
 import Alamofire
 
+let API_URL: String = "https://podcast-service.devpython.ru/api"
+
 enum AuthenticationError: Error {
     case invalidCredentials
     case custom(errorMessage: String)
@@ -16,6 +18,10 @@ enum NetworkError: Error{
 struct LoginRequestBody: Codable{
     let email: String
     let password: String
+}
+
+struct RefreshTokenRequestBody: Codable{
+    let refresh_token: String
 }
 
 struct LoginResponseBody: Codable{
@@ -52,14 +58,98 @@ struct Episode: Decodable, Hashable{
 }
 
 
-class AccessTokenAdapter: RequestAdapter {
-    private let accessToken: String
-
-    init(accessToken: String) {
-        self.accessToken = accessToken
+class AccessTokenInterceprot: RequestInterceptor{
+    private let retryLimit = 5;
+    
+    func getToken(tokenType: String = "accessToken") -> String?{
+        let keychain = Keychain(service: "com.podcast")
+        guard let token = try? keychain.get(tokenType) else {
+            print("No access token found")
+            return nil
+        }
+        return token
     }
 
-//    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+    func refreshToken(completion: @escaping (_ isSuccess: Bool) -> Void) {
+        guard let refreshToken = self.getToken(tokenType: "refreshToken") else { return }
+        guard let url = URL(string: "\(API_URL)/auth/sign-in/") else {
+            return
+        }
+        // todo: using AF.request instead
+        let body = RefreshTokenRequestBody(refresh_token: refreshToken)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        URLSession.shared.dataTask(with: request){ (data, resp, error) in
+            guard let data = data, error == nil else {
+                print("\nRefresh token failed: 'No data in response'")
+                completion(false)
+                return
+            }
+            guard let response = try? JSONDecoder().decode(LoginResponseBody.self, from: data) else {
+                print("\nRefresh token failed: 'Unable to decode response JSON'")
+                completion(false)
+                return
+            }
+            let keychain = Keychain(service: "com.podcast")
+            do {
+                try keychain.set(response.payload.access_token, key: "accessToken")
+                try keychain.set(response.payload.refresh_token, key: "refreshToken")
+            }
+            catch let error {
+                print("Couldn't save access/refresh tokens: \(error)")
+            }
+            completion(true)
+            
+        }.resume()
+        
+    }
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var request = urlRequest
+        guard let token = self.getToken() else { return }
+        let bearerToken = "Bearer \(token)"
+        request.setValue(bearerToken, forHTTPHeaderField: "Authorization")
+        print("\nadapted; token added to the header field is: \(bearerToken)\n")
+        completion(.success(request))
+    }
+    
+        
+    func retry(_ request: Request, for session: Session, dueTo error: Error,
+                  completion: @escaping (RetryResult) -> Void) {
+        guard request.retryCount < retryLimit else {
+            completion(.doNotRetry)
+            return
+        }
+        print("\nretried; retry count: \(request.retryCount)\n")
+        refreshToken { isSuccess in
+            isSuccess ? completion(.retry) : completion(.doNotRetry)
+        }
+    }
+
+}
+
+
+//class AccessTokenAdapter: RequestAdapter {
+//    private let accessToken: String
+//
+//    init(accessToken: String) {
+//        self.accessToken = accessToken
+//    }
+//
+////    func adapt(_ urlRequest: URLRequest) throws -> URLRequest {
+////        var urlRequest = urlRequest
+////
+////        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix("https://httpbin.org") {
+////            urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
+////        }
+////
+////        return urlRequest
+////    }
+//
+//    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
 //        var urlRequest = urlRequest
 //
 //        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix("https://httpbin.org") {
@@ -68,17 +158,7 @@ class AccessTokenAdapter: RequestAdapter {
 //
 //        return urlRequest
 //    }
-    
-    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        var urlRequest = urlRequest
-
-        if let urlString = urlRequest.url?.absoluteString, urlString.hasPrefix("https://httpbin.org") {
-            urlRequest.setValue("Bearer " + accessToken, forHTTPHeaderField: "Authorization")
-        }
-
-        return urlRequest
-    }
-}
+//}
 
 
 class WebService{
@@ -148,9 +228,28 @@ class WebService{
         }.resume()
     }
     
-    
     func getPodcasts(limit: Int = 20, completion: @escaping (Result<[PodcastItem], NetworkError>) -> Void){
-        guard let url = URL(string: "\(APIUrl)/podcasts/") else {
+        let interceptor: AccessTokenInterceprot = AccessTokenInterceprot()
+        AF.request("\(API_URL)/podcasts/", interceptor: interceptor).response { response in
+            debugPrint(response)
+            guard let data = response.data else {
+                completion(.failure(.noData))
+                return
+            }
+            guard let podcastResponse = try? JSONDecoder().decode(PodcastsListResponse.self, from: data) else {
+                completion(.failure(.decodingError))
+                return
+            }
+            let podcasts = podcastResponse.payload
+            completion(.success(podcasts))
+
+
+        }
+    }
+    
+    
+    func getPodcastsOld(limit: Int = 20, completion: @escaping (Result<[PodcastItem], NetworkError>) -> Void){
+        guard let url = URL(string: "\(API_URL)/podcasts/") else {
             completion(.failure(.invalidURL))
             return
         }
