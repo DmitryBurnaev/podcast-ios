193d22bab3290c92fdebf43dd11fd8a42d759b9a
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-10-11 08:57:39 +0300

    [#32] Episode's creation: project's constatnts

--- src/podcast/constants.swift
+++ src/podcast/constants.swift
@@ -1,4 +1,4 @@
 import Foundation
 
-let API_URL: String = "http://192.168.1.3:8001/api"
-let TEST_PODCAST_ID: Int = 1
+let API_URL: String = ProcessInfo.processInfo.environment["API_URL"] ?? "http://192.168.1.3:8001/api"
+let TEST_PODCAST_ID: Int = 9999
