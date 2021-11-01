d50c8a83ca42b583c07a3ef5b82aad8c73074583
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-11-01 11:45:33 +0300

    [#27] Implementation for sharing ext (step 2.3)

--- src/podcast/ShareViewController.swift
+++ src/podcast/ShareViewController.swift
@@ -9,13 +9,23 @@ class ShareViewController: SLComposeServiceViewController {
 
     }
 
-//    open var textView: UITextView! { get }
+    override func viewDidLoad() {
+        print("viewDidLoad")
+        super.viewDidLoad()
+        self.handleSharedLink()
+    }
+    
+    private func handleSharedLink() {
+        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
+        print(attachments)
+    }
 
     override func didSelectPost() {
+        print("didSelectPost")
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
         let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
         print(attachments)
-        
+
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
@@ -26,3 +36,25 @@ class ShareViewController: SLComposeServiceViewController {
     }
 
 }
+//import UIKit
+//import MobileCoreServices
+//
+//@objc(ShareExtensionViewController)
+//class ShareViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.handleSharedLink()
+//    }
+//
+//    private func handleSharedLink() {
+//        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
+//        print(attachments)
+//    }
+//
+////    override func configurationItems() -> [Any]! {
+////        super.configurationItems()
+////        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+////        return []
+////    }
+//}
