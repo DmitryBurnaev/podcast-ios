8b0d95ede2b16eba5f0d04dec5d4027a806ae2ce
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-11-08 09:30:50 +0300

    [#27] Implementation for sharing ext (step 2.8)

--- src/podcast/ShareViewController.swift
+++ src/podcast/ShareViewController.swift
@@ -31,63 +31,39 @@ class ShareViewController: SLComposeServiceViewController {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
         let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
         print(attachments)
-
+        //  TODO: copy link to clipboard -> open app
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
 
-    override func configurationItems() -> [Any]! {
-        var configurations: [SLComposeSheetConfigurationItem] = []
-        var podcasts: [PodcastInList] = []
-        podcasts = try! self.getPodcasts()
-//        do{
-//
-//        } catch{
-//            podcasts = []
+//    override func configurationItems() -> [Any]! {
+//        var configurations: [SLComposeSheetConfigurationItem] = []
+//        var podcasts: [PodcastInList] = []
+//        podcasts = try! self.getPodcasts()
+//        for podcast in podcasts{
+//            let c = SLComposeSheetConfigurationItem()!
+//            c.title = podcast.name
+//            c.value = String(podcast.id)
+//            configurations.append(c)
 //        }
-//
-        for podcast in podcasts{
-            let c = SLComposeSheetConfigurationItem()!
-            c.title = podcast.name
-            c.value = String(podcast.id)
-            configurations.append(c)
-        }
-        return configurations
-    }
+//        return configurations
+//    }
     
     
     func getPodcasts() throws -> [PodcastInList]{
-        let defaults = UserDefaults.standard
-        let decoder = JSONDecoder()
-//        let srcData = String(data: defaults.object(forKey: "podcasts") as! Data, encoding: .utf8)
-        let srcData = defaults.object(forKey: "podcasts") as! Data
-        print("Defaults \(srcData)")
-//        let string = String(data: data, encoding: .utf8)!
-        let podcasts: [PodcastInList] = try decoder.decode([PodcastInList].self, from: srcData)
-        return podcasts
+        // TODO: get real podcasts here
+        return [
+            PodcastInList(id: 1, name: "My Podcast 1"),
+            PodcastInList(id: 2, name: "Test Podcast2"),
+        ]
+        
+//        let defaults = UserDefaults.standard
+//        let decoder = JSONDecoder()
+//        let srcData = defaults.object(forKey: "podcasts") as! Data
+//        print("Defaults \(srcData)")
+//        let podcasts: [PodcastInList] = try decoder.decode([PodcastInList].self, from: srcData)
+//        return podcasts
     }
     
 
 }
-//import UIKit
-//import MobileCoreServices
-//
-//@objc(ShareExtensionViewController)
-//class ShareViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.handleSharedLink()
-//    }
-//
-//    private func handleSharedLink() {
-//        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
-//        print(attachments)
-//    }
-//
-////    override func configurationItems() -> [Any]! {
-////        super.configurationItems()
-////        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-////        return []
-////    }
-//}
