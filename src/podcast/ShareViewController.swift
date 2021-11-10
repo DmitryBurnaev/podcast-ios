302d5e5f940d3cd0ecd22013f05699dc5970b74e
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-11-10 10:03:56 +0300

    [#27] Implementation for sharing ext (step 2.9)

--- src/podcast/ShareViewController.swift
+++ src/podcast/ShareViewController.swift
@@ -27,14 +27,36 @@ class ShareViewController: SLComposeServiceViewController {
     }
 
     override func didSelectPost() {
-        print("didSelectPost")
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
-        print(attachments)
-        //  TODO: copy link to clipboard -> open app
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = item.attachments?.first {
+                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) -> Void in
+                        if let shareURL = url as? NSURL {
+                            print(shareURL)
+                            // todo: open contaiting app https://habr.com/ru/company/mobileup/blog/441890/
+                        }
+                        self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
+                    })
+                }
+            }
+        }
     }
+//
+//
+//    override func didSelectPost() {
+//        print("didSelectPost")
+//        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+//        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
+//        print(attachments)
+//        var link: String = ""
+//        for attachment in attachments{
+//            print("attachment \(attachment)")
+//            link = attachment.description
+//        }
+//        //  TODO: copy link to clipboard -> open app
+//        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+//        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+//    }
 
 //    override func configurationItems() -> [Any]! {
 //        var configurations: [SLComposeSheetConfigurationItem] = []
