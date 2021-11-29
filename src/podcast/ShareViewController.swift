1c78453305e9524bdb2fd9de80009da72208547f
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-11-29 19:26:07 +0300

    [#27] Implementation for sharing ext (step 2.27)

--- src/podcast/ShareViewController.swift
+++ src/podcast/ShareViewController.swift
@@ -1,12 +1,12 @@
+//
+//  ShareViewController.swift
+//  Podcast
+//
+//  Created by Dmitry Burnaev on 29.11.2021.
+//
+
 import UIKit
 import Social
-import podcast
-import SwiftUI
-
-struct PodcastInList: Codable, Hashable {
-    let id: Int
-    let name: String
-}
 
 class ShareViewController: SLComposeServiceViewController {
 
@@ -15,60 +15,16 @@ class ShareViewController: SLComposeServiceViewController {
         return true
     }
 
-//    override func viewDidLoad() {
-//        print("viewDidLoad")
-//        super.viewDidLoad()
-//        self.handleSharedLink()
-//    }
-//
-//    private func handleSharedLink() {
-//        let attachments = (self.extensionContext?.inputItems.first as? NSExtensionItem)?.attachments ?? []
-//    }
-
     override func didSelectPost() {
-        guard let url = URL(string: "podcast://") else { return }
-        print(url)
-        self.extensionContext?.open(url)
-        
-//        if let item = extensionContext?.inputItems.first as? NSExtensionItem {
-//            if let itemProvider = item.attachments?.first {
-//                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-//                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) -> Void in
-//                        if let shareURL = url as? NSURL {
-////                            guard let url = URL(string: "PodcastAppURL://") else { return }
-//                            guard let url = URL(string: "podcast://") else { return }
-//                            print(url)
-//                            
-//                            print(self.extensionContext)
-////                            UIApplication.open
-////                            UIApplication.shared.open(url)
-//                            self.extensionContext?.open(url)
-////                            self.extensionContext?.open(url, completionHandler: {(_) -> Void in
-////                                print("URL opened!")
-////                            })
-//                            // todo: open contaiting app https://habr.com/ru/company/mobileup/blog/441890/
-//                        }
-//                        self.extensionContext?.completeRequest(returningItems: [], completionHandler:nil)
-//                    })
-//                }
-//            }
-//        }
-    }
+        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
-    func getPodcasts() throws -> [PodcastInList]{
-        // TODO: get real podcasts here
-        return [
-            PodcastInList(id: 1, name: "My Podcast 1"),
-            PodcastInList(id: 2, name: "Test Podcast2"),
-        ]
-        
-//        let defaults = UserDefaults.standard
-//        let decoder = JSONDecoder()
-//        let srcData = defaults.object(forKey: "podcasts") as! Data
-//        print("Defaults \(srcData)")
-//        let podcasts: [PodcastInList] = try decoder.decode([PodcastInList].self, from: srcData)
-//        return podcasts
+        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
-    
+
+    override func configurationItems() -> [Any]! {
+        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+        return []
+    }
 
 }
