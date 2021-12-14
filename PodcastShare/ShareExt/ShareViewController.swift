7f76f6b3f3f2562997ba5b4a79aa764cbca6989b
DmitryBurnaev <dmitry.burnaev@gmail.com>
2021-12-14 10:19:37 +0300

    [#27] Implementation for sharing ext: new app (step 2.34)

--- PodcastShare/ShareExt/ShareViewController.swift
+++ PodcastShare/ShareExt/ShareViewController.swift
@@ -1,31 +1,113 @@
 import UIKit
 import Social
+import CoreServices
 
-class ShareViewController: SLComposeServiceViewController {
+//class ShareViewController: SLComposeServiceViewController {
+//
+//    override func isContentValid() -> Bool {
+//        // Do validation of contentText and/or NSExtensionContext attachments here
+//        return true
+//    }
+//
+//    override func didSelectPost() {
+//        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+//
+//        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+//        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+//    }
+//
+//    override func configurationItems() -> [Any]! {
+//        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+//        return []
+//    }
+//
+//}
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
+
+class ShareViewController: UIViewController {
+    
+    private let typeText = String(kUTTypeText)
+    private let typeURL = String(kUTTypeURL)
+
+    // Courtesy: https://stackoverflow.com/a/44499222/13363449 👇🏾
+    // Function must be named exactly like this so a selector can be found by the compiler!
+    // Anyway - it's another selector in another instance that would be "performed" instead.
+    @objc func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                return application.perform(#selector(openURL(_:)), with: url) != nil
+            }
+            responder = responder?.next
+        }
+        return false
     }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
 
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Get the all encompasing object that holds whatever was shared. If not, dismiss view.
+        guard let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+            let itemProvider = extensionItem.attachments?.first else {
+                self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+                return
+        }
+
+        // Check if object is of type text
+        if itemProvider.hasItemConformingToTypeIdentifier(typeText) {
+            handleIncomingText(itemProvider: itemProvider)
+        // Check if object is of type URL
+        } else if itemProvider.hasItemConformingToTypeIdentifier(typeURL) {
+            handleIncomingURL(itemProvider: itemProvider)
+        } else {
+            print("Error: No url or text found")
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    private func handleIncomingText(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeText, options: nil) { (item, error) in
+            if let error = error {
+                print("Text-Error: \(error.localizedDescription)")
+            }
+
+            if let text = item as? String {
+                do {
+                    // Detect URLs in String
+                    let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+                    let matches = detector.matches(
+                        in: text,
+                        options: [],
+                        range: NSRange(location: 0, length: text.utf16.count)
+                    )
+                    // Get first URL found
+                    if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
+                        print(text[range])
+                    }
+                } catch let error {
+                    print("Do-Try Error: \(error.localizedDescription)")
+                }
+            }
+
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
     }
 
+    private func handleIncomingURL(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeURL, options: nil) { (item, error) in
+            if let error = error {
+                print("URL-Error: \(error.localizedDescription)")
+            }
+
+            if let url = item as? NSURL, let urlString = url.absoluteString {
+                print(urlString)
+            }
+
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
+    }
+    
+    
 }
 
-//
-//class ShareViewController: UIViewController {
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
-//    }
-//}
