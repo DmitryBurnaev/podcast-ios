79cbd3146e64659cedacf268ff7b7275393a64aa
DmitryBurnaev <dmitry.burnaev@gmail.com>
2022-01-03 17:12:07 +0300

    [#27] added small fixes for share text/link logic

--- src/podcast/constants.swift
+++ src/podcast/constants.swift
@@ -1,4 +1,5 @@
 import Foundation
 
-let API_URL: String = ProcessInfo.processInfo.environment["API_URL"] ?? "http://192.168.1.3:8001/api"
+//let API_URL: String = ProcessInfo.processInfo.environment["API_URL"] ?? "http://192.168.1.3:8001/api"
+let API_URL: String = "https://podcast-service.devpython.ru/api"
 let TEST_PODCAST_ID: Int = 9999
--- src/shareExt/ShareViewController.swift
+++ src/shareExt/ShareViewController.swift
@@ -4,9 +4,10 @@ import CoreServices
 
 
 class ShareViewController: UIViewController {
-    private var appURLString = "podcast://"
+    private var appURLString = "podcast.dev://"
+    private let typeText = String(kUTTypeText)
     private let typeURL = String(kUTTypeURL)
-    
+
     @objc func openURL(_ url: URL) -> Bool {
         var responder: UIResponder? = self
         while responder != nil {
@@ -27,10 +28,13 @@ class ShareViewController: UIViewController {
                 self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
                 return
         }
+        // Check if object is of type text
+        if itemProvider.hasItemConformingToTypeIdentifier(typeText) {
+            handleIncomingText(itemProvider: itemProvider)
         // Check if object is of type URL
-        if itemProvider.hasItemConformingToTypeIdentifier(typeURL) {
-            handleIncomingURL(itemProvider: itemProvider)
-        } else {
+        } else if itemProvider.hasItemConformingToTypeIdentifier(typeURL) {
+            handleIncomingURL(itemProvider: itemProvider)        }
+        else {
             print("Error: No url found")
             self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
         }
@@ -51,6 +55,39 @@ class ShareViewController: UIViewController {
             self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
         }
     }
+    
+    private func handleIncomingText(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeText, options: nil) { (item, error) in
+            if let error = error {
+                print("Text-Error: \(error.localizedDescription)")
+            }
+
+            if let text = item as? String {
+                do {
+                    // Detect URLs in String
+                    let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+                    let matches = detector.matches(
+                        in: text,
+                        options: [],
+                        range: NSRange(location: 0, length: text.utf16.count)
+                    )
+                    // Get first URL found
+                    if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
+                        print(text[range])
+                        if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
+                            self.saveURLString(String(text[range]))
+                            self.openMainApp()
+                        }
+                    }
+                } catch let error {
+                    print("Do-Try Error: \(error.localizedDescription)")
+                }
+            }
+
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
+    }
+    
     private func openMainApp() {
         self.extensionContext?.completeRequest(returningItems: nil, completionHandler: { _ in
             guard let url = URL(string: self.appURLString) else { return }
